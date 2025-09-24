17a8e93f35e2a6d36253453e1b96f1261b07f0d5
mbale014 <195115650+mbale014@users.noreply.github.com>
2025-09-24 13:05:44 +0800

    fix ui logic bug and organize html structure

--- script.js
+++ script.js
@@ -6,13 +6,14 @@ const operators = {
     '-' : function (a,b) {return a - b},
     '*' : function (a,b) {return a * b},
     '/' : function (a,b) {return a / b},
-}
+};
 
 // operate variables
-let valueStored = '';
-let number1 = '';
-let number2 = '';
+let valueTemp = '';
+let beforeNumber = '';
+let afterNumber = '';
 let operatorFunc = '';
+let isOnDigit = false;
 
 // execute operator function
 function operate(operatorProp, num1, num2) {
@@ -22,58 +23,75 @@ function operate(operatorProp, num1, num2) {
         return operators[operatorProp](num1, num2);
     }
     
-}
+};
 
 // Click to populate the display function
 function populateDisplay() {
     const display = document.querySelector('.display input');
-    const buttons = document.querySelectorAll('.btn-group button');
+    const btnDigit = document.querySelectorAll('.digits-btn button');
+    const btnClear = document.querySelector('.clear-btn button');
+    const btnOperator = document.querySelectorAll('.operator-btn button');
+    const btnEqual = document.querySelector('.equal-sign-operator');
 
-    buttons.forEach(item => {
+    // Add digit on every digits button click to show it on display
+    btnDigit.forEach(item => {
         item.addEventListener('click', (e) => {
-            number2 = '';
-            if (e.target.parentNode.className === 'clear-btn') {
-                // clear display value to empty string
-                number1 = number2 = operatorFunc = valueStored = display.value = '';
-            } else if (e.target.className === 'operate-calc-btn') {
-                // execute operate function number 1 with number 2 when '=' clicked 
-                number2 = display.value;
-                const calcResult = operate(operatorFunc, +number1, +number2);
-                number1 = calcResult;
-                number2 = '';
-                valueStored = display.value = calcResult;
-            } else if (e.target.parentNode.className === 'operator-btn' && e.target.className !== 'operate-calc-btn') {
-                // When operator clicked, prompt user for the second number or show the result before
-                if (number1 === '') {
-                    number1 = display.value;
-                    display.value = '';
-                    operatorFunc = e.target.textContent;
-                } else if (number1 !== '') {
-                    number2 = display.value;
-                    const calcResult = operate(operatorFunc, +number1, +number2);
-                    display.value = calcResult;
-                    valueStored = calcResult;
-                } else {
-                    e.preventDefault();
-                }
-                
-            } else {
-                // input digit by adding to display
-                if (valueStored !== '') {
-                    valueStored = '';
-                    display.value = '';
-                    const btnText = e.target.textContent;
-                    display.value += btnText;
-                } else {
-                    const btnText = e.target.textContent;
-                    display.value += btnText;
-                }
-                
-                console.log(valueStored, number1, number2, operatorFunc);
+            isOnDigit = true;
+            if (valueTemp !== '') {
+                valueTemp = '';
+                display.value = '';
             }
+            const digitValue = e.target.textContent;
+            display.value += digitValue;
+            console.log(valueTemp, beforeNumber, afterNumber, operatorFunc);
         })
+    });
+
+    // Clear everything on clear button click
+    btnClear.addEventListener('click', () => {
+        display.value = valueTemp = beforeNumber = afterNumber = operatorFunc = '';
+        console.log(valueTemp, beforeNumber, afterNumber, operatorFunc);
+    });
+
+    // Make the operator button works on click
+    btnOperator.forEach(item => {
+        item.addEventListener('click', (e) => {
+            if (e.target.className !== 'equal-sign-operator') {
+                valueTemp = display.value;
+                operatorFunc = '';
+                if (!isOnDigit) {
+                    return;
+                }else if (beforeNumber !== '') {
+                    afterNumber = valueTemp;
+                    operatorFunc = e.target.textContent; // store the operator for next operation
+                    const result = operate(operatorFunc, +beforeNumber, +afterNumber); // save the operate between number and save to result var
+                    display.value = result; // show the result on display
+                    beforeNumber = result;
+                    isOnDigit = false;
+                } else if (beforeNumber === '') {
+                    // second condition when user resets or first time using operator
+                    beforeNumber = valueTemp;
+                    operatorFunc = e.target.textContent;
+                    isOnDigit = false;
+                }
+                console.log(valueTemp, beforeNumber, afterNumber, operatorFunc);
+            }
+        }); 
+    });
+
+    // Make the equal button works on click to operate
+    btnEqual.addEventListener('click', () => {
+        if (!isOnDigit) {
+            return;
+        };
+        afterNumber = display.value;
+        const resultEqual = operate(operatorFunc, +beforeNumber, +afterNumber); // save the operate between number and save to result var
+        display.value = resultEqual; // show the result on display
+        beforeNumber = afterNumber = operatorFunc = valueTemp = '';
+        isOnDigit = false;
+        console.log(valueTemp, beforeNumber, afterNumber, operatorFunc);
     })
-}
+};
 
 populateDisplay();
 
